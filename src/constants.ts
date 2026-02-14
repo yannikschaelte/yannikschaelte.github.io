90ce1a0d27a0535cc044205ebb18d9a2a72fa82b
yannikschaelte <yannik.schaelte@gmail.com>
2026-02-15 00:35:33 +0100

    Add GitHub link and update Twitter to X in social icons

--- src/constants.ts
+++ src/constants.ts
@@ -3,6 +3,7 @@ export const SITE_DATA = {
     tagline: "AI Scientist | Developer | Mathematician",
     twitter: "yannik_schaelte",
     linkedin: "yannik-schaelte",
+    github: "yannikschaelte",
     scholar: "xpdW7JUAAAAJ",
     email: "firstname.lastname [at] gmail.com",
 };
